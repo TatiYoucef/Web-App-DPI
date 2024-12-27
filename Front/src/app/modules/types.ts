2304a1cfd1b9fa8a7cbda8c9e8259a26d00fc6d0
Ouadi Chaima <mc_ouadi@esi.dz>
2024-12-27 01:26:39 +0100

    just defined the types hada makan

--- Front/src/app/modules/types.ts
+++ Front/src/app/modules/types.ts
@@ -45,16 +45,33 @@ export type TestBilan = {
 
 }
 
-export type Ordonnance = {
+export type DPI = {
   id : number,
-  dateCreation : String,
-  duree : String,
-  etat : Boolean,
-  medicaments : Medicament[] | null,
+  id_medecin : number,
+  date_maj : Date,
+  date_admission : Date,
+  date_sortie : Date,
+  antecedents_medicaux : String[],
+
+// antécédents médicaux : String[]
+// ordonnances : Ordonnance[]
+// bilans : Bilan[]
+// observations : Observation[]
+// soins : Soin[]
 
 }
 
+export type Ordonnance = {
+  id : number,
+  id_DPI : number,
+  dateCreation : Date,
+  duree : String,
+  etat : Boolean,
+}
+
 export type Medicament = {
+  id : number,
+  id_ordonnance : number,
   nom : String,
   dose : String,
   frequence : String,
