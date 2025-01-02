90a13a28df839421e6153071b4d2ddf8349e33d4
Ouadi Chaima <mc_ouadi@esi.dz>
2025-01-02 02:01:15 +0100

    Done today: affichage des ordonnances, chacune ses médicaments. To do: formulaires de l'ajout d'une ordonnance/un médicament + routes

--- Front/src/app/pages/supplementPages/ordonnances/ordonnances-accueil/ordonnances-accueil.component.ts
+++ Front/src/app/pages/supplementPages/ordonnances/ordonnances-accueil/ordonnances-accueil.component.ts
@@ -4,20 +4,69 @@ import { HeaderComponent } from "../../../../components/header-user/header.compo
 import { DashBoardComponent } from "../../../../components/dash-board/dash-board.component";
 import { LoadingScreenComponent } from "../../../../components/loading-screen/loading-screen.component";
 import { CommonModule } from '@angular/common'; 
-import { FetchModulesService } from '../../../../services/fetchModules/fetch-modules.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-ordonnances-accueil',
   standalone: true,
-  imports: [HeaderComponent, DashBoardComponent, LoadingScreenComponent, CommonModule],
+  imports: [HeaderComponent, DashBoardComponent, LoadingScreenComponent, CommonModule, FormsModule],
   templateUrl: './ordonnances-accueil.component.html',
   styleUrl: './ordonnances-accueil.component.css'
 })
 export class OrdonnancesAccueilComponent {
 
   isDashBoard = signal(false);
+  isDashBoardVisible = true;
+  ordonnances2 = [];
+  ordonnances = [
+    {
+      "id": 23,
+      "id_DPI": 12,
+      "dateCreation": "2024-12-11",
+      "duree": "5 jours",
+      "etat": true,
+      "medicaments": [
+        {
+          "id": 34,
+          "id_ordonnance": 23,
+          "nom": "Salbutamol",
+          "dose": "2 inhalations",
+          "frequence": "au besoin"
+        }
+      ]
+    },
+    {
+      "id": 24,
+      "id_DPI": 12,
+      "dateCreation": "2024-12-13",
+      "duree": "10 jours",
+      "etat": false,
+      "medicaments": [
+        {
+          "id": 35,
+          "id_ordonnance": 24,
+          "nom": "Montélukast",
+          "dose": "10mg",
+          "frequence": "1 fois par jour"
+        },
+        {
+          "id": 36,
+          "id_ordonnance": 24,
+          "nom": "Fluticasone",
+          "dose": "2 inhalations",
+          "frequence": "2 fois par jour"
+        }
+      ]
+    }
+  ]
   changeDashState(){
     this.isDashBoard.update((e) => !e);
+  }
+
+  toggleEtat(ordonnance: any): void {
+    ordonnance.etat = !ordonnance.etat;
+    console.log('Etat changed to:', ordonnance.etat);
   }
 
   router = inject(Router);
